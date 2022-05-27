00eed534815fbad8befcb8554720cdd181e0c04b
Claude Roy <claude.roy790@gmail.com>
2022-05-26 23:15:41 -0500

    update styles

--- daily-verse/src/app/bible/bible-routing.module.ts
+++ daily-verse/src/app/bible/bible-routing.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { BiblePage } from './bible.page';
 
